5360e27ac6b8b40ebed479fc49e36e90ed44b2d5
lambda <lambda@lambda.lm>
2018-02-26 17:30:36 +0700

    циклический вызов апи докера

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+import logging
 import asyncssh
 from pathlib import Path
 from terminaltables import AsciiTable
@@ -10,7 +11,7 @@ STS_RUNNNING = ' -f "status=running"'
 STS_OTHERS = ' -f "status=created" -f "status=restarting" -f "status=removing" -f "status=paused" -f "status=exited"' \
              ' -f "status=dead"'
 CMD = 'docker ps --format="{{.Names}}%s{{.Ports}}%s{{.Status}}%s{{.Image}}"' % (SEPARATOR, SEPARATOR, SEPARATOR)
-
+#--format='{{(index .Spec.EndpointSpec.Ports 0).PublishedPort}}'
 
 def get_creds_from_config(file_name):
     creds = []
@@ -25,19 +26,18 @@ def get_creds_from_config(file_name):
                 host = line.split()[1]
             elif line.startswith('User'):
                 user = line.split()[1]
-                if user == 'root' and not pass_:
+                if not pass_ and user == 'root':
                     creds.append((host, user))
     return creds
 
 
-async def container_info(host, user, others=False):
+async def container_info(host, conn, others=False):
     table_data = []
     try:
         cmd = CMD + STS_RUNNNING
         if others:
             cmd = CMD + STS_OTHERS
 
-        conn = await asyncio.wait_for(asyncssh.connect(host, username=user), timeout=10)
         result = await asyncio.wait_for(conn.run(cmd, check=True), timeout=10)
 
         lines = result.stdout.splitlines()
@@ -50,35 +50,44 @@ async def container_info(host, user, others=False):
             line = [''] + line.split(SEPARATOR)
             table_data.append(line)
     except Exception as e:
-        print(e)
-        print(host, user)
+        logging.exception('')
 
     return table_data
 
 
 async def main(all_=False):
     creds = get_creds_from_config('~/.ssh/config')
-    table_data = [['Host', 'Status', 'Names', 'Ports', 'Image'], ]
+    done, pending = await asyncio.wait([asyncssh.connect(host, username=user, client_keys=['/home/owner/.ssh/gitqpay'])
+                                        for host, user in creds],
+                                       timeout=10)
+    connections = {}
+    for t in done:
+        conn = t.result()
+        connections[conn._host] = conn
 
-    f = asyncio.gather(*[container_info(host, user) for host, user in creds])
-    results = await asyncio.wait_for(f, None)
-    for r in results:
-        table_data.extend(r)
-
-    if all_:
-        table_data.append(['', '', '', '', ''])
-        table_data.append(['', '', '', '', ''])
-        f = asyncio.gather(*[container_info(host, user, others=True) for host, user in creds])
+    while True:
+        table_data = [['Host', 'Names', 'Ports', 'Status', 'Image'], ]
+        f = asyncio.gather(*[container_info(host, conn) for host, conn in connections.items()])
         results = await asyncio.wait_for(f, None)
         for r in results:
             table_data.extend(r)
 
-    table = AsciiTable(table_data)
-    print(table.table)
+        if all_:
+            table_data.append(['', '', '', '', ''])
+            table_data.append(['', '', '', '', ''])
+            f = asyncio.gather(*[container_info(host, user, others=True) for host, user in creds])
+            results = await asyncio.wait_for(f, None)
+            for r in results:
+                table_data.extend(r)
+
+        table = AsciiTable(table_data)
+        print(table.table)
+        await asyncio.sleep(60)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--all', action='store_true', dest='all')
     args = parser.parse_args()
+    logging.basicConfig()
     asyncio.get_event_loop().run_until_complete(main(args.all))
