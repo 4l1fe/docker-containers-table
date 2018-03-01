7060147b9e145e40d074e7f50f64c189941ba625
lambda <lambda@lambda.lm>
2018-03-01 20:34:48 +0700

    расширение входных параметров

--- run.py
+++ run.py
@@ -7,9 +7,6 @@ from terminaltables import AsciiTable
 from containers import ContainerInfo
 
 
-CLIENT_KEY = '/home/owner/.ssh/gitqpay'
-
-
 def get_creds_from_config(file_name):
     creds = []
     with open(Path(file_name).expanduser().as_posix(), 'r') as file:
@@ -28,9 +25,9 @@ def get_creds_from_config(file_name):
     return creds
 
 
-async def main(all_=False, port_forward=False):
-    creds = get_creds_from_config('~/.ssh/config')
-    done, pending = await asyncio.wait([asyncssh.connect(host, username=user, client_keys=[CLIENT_KEY])
+async def main(config_file, all_=False, client_keys=(), port_forward=False):
+    creds = get_creds_from_config(config_file)
+    done, pending = await asyncio.wait([asyncssh.connect(host, username=user, client_keys=client_keys)
                                         for host, user in creds],
                                        timeout=10)
     connections = {}
@@ -60,8 +57,10 @@ async def main(all_=False, port_forward=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config-file',  default='~/.ssh/config')
+    parser.add_argument('-pk', '--private-key')
     parser.add_argument('--all', action='store_true', dest='all')
     parser.add_argument('--fwd', action='store_true', dest='port_forward')
     args = parser.parse_args()
     logging.basicConfig()
-    asyncio.get_event_loop().run_until_complete(main(args.all, args.port_forward))
+    asyncio.get_event_loop().run_until_complete(main(args.config_file, args.all, [args.private_key], args.port_forward))
