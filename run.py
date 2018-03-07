21648d7d914cff89f87e1e7374a2f6106ab2dba1
lambda <lambda@lambda.lm>
2018-03-07 17:49:22 +0700

    форвардинг по инкр адресу относительно хоста

--- run.py
+++ run.py
@@ -4,20 +4,21 @@ import logging
 import asyncssh
 from pathlib import Path
 from itertools import chain
+from ipaddress import IPv4Network
+from collections import defaultdict
 from terminaltables import AsciiTable
 from containers import ContainerInfo
 
 
-FWD_HOST = '127.0.0.1'
-FWD_PORT_BASE = 10000
+FWD_NETWORK = IPv4Network('127.0.0.0/24')
 TIMEOUT = 60
 CONN_TIMEOUT = 10
 CONFIG_FILE = '~/.ssh/config'
 
 
-def get_creds_from_config(file_name):
-    creds = []
-    with open(Path(file_name).expanduser().as_posix(), 'r') as file:
+def get_host_user_pairs(config_file):
+    host_user_pairs = []
+    with open(Path(config_file).expanduser().as_posix(), 'r') as file:
         for line in file.readlines():
             if not line.strip(): continue
 
@@ -29,49 +30,72 @@ def get_creds_from_config(file_name):
             elif line.startswith('User'):
                 user = line.split()[1]
                 if not pass_ and user == 'root':
-                    creds.append((host, user))
-    return creds
+                    host_user_pairs.append((host, user))
+
+    return host_user_pairs
 
 
-async def start_forwarding(ci):
-    pport = FWD_PORT_BASE + ci.public_port
+async def _listen_forwaded(ci, fwd_host):
     try:
-        listener = await asyncio.wait_for(ci.connection.forward_local_port(FWD_HOST, pport, ci.public_host, ci.public_port),
+        listener = await asyncio.wait_for(ci.connection.forward_local_port(str(fwd_host), ci.public_port,
+                                                                           ci.public_host, ci.public_port),
                                           timeout=CONN_TIMEOUT)
         await listener.wait_closed()
     except:
         logging.exception('')
 
 
-async def main(config_file, all_=False, client_keys=(), port_forward=False, timeout=TIMEOUT):
-    creds = get_creds_from_config(config_file)
-    done, pending = await asyncio.wait([asyncssh.connect(host, username=user, client_keys=client_keys)
-                                        for host, user in creds],
-                                       timeout=CONN_TIMEOUT)
+def start_forwarding(ci_list):
+
+    hosts = FWD_NETWORK.hosts()
+    def set_host():
+        return next(hosts)
+
+    fwd_hosts = defaultdict(set_host)
+    for ci in ci_list:
+        if not ci.public_host or ci.public_host == '0.0.0.0':
+            continue
+
+        fwd_host = fwd_hosts[ci.host]
+        asyncio.ensure_future(_listen_forwaded(ci, fwd_host))
+
+
+async def make_connections(hu_pairs, client_keys=()):
     connections = {}
+    done, pending = await asyncio.wait([asyncssh.connect(host, username=user, client_keys=client_keys)
+                                        for host, user in hu_pairs],
+                                       timeout=CONN_TIMEOUT)
     for t in done:
         conn = t.result()
         connections[conn._host] = conn
 
+    return connections
+
+
+async def main(config_file, all_=False, client_keys=(), port_forward=False, timeout=TIMEOUT):
+    hu_pairs = get_host_user_pairs(config_file)
+
+    connections = await make_connections(hu_pairs, client_keys)
+
     while True:
         print("\033c")
-        table_data = [['Host', 'Names', 'Ports', 'Status', 'Image'], ]
+
         f = asyncio.gather(*[ContainerInfo.get_all(host, conn) for host, conn in connections.items()])
-        results = await asyncio.wait_for(f, None)
-        table_data.extend((ci.host, ci.name, ci.ports, ci.status, ci.image) for ci in chain(*results))
+        ci_batches = await asyncio.wait_for(f, None)
+
+        table_data = [['Host', 'Names', 'Ports', 'Status', 'Image'], ]
+        table_data.extend((ci.host, ci.name, ci.ports, ci.status, ci.image) for ci in chain(*ci_batches))
 
         if port_forward:
-            for ci in chain(*results):
-                if ci.public_host and ci.public_host != '0.0.0.0':
-                    asyncio.ensure_future(start_forwarding(ci))
+            start_forwarding(chain(*ci_batches))
             port_forward = False  # разовое установление перенаправления
 
         if all_:
             table_data.append(['', '', '', '', ''])
             table_data.append(['', '', '', '', ''])
-            f = asyncio.gather(*[ContainerInfo.get_all(host, user, others=True) for host, user in creds])
-            results = await asyncio.wait_for(f, None)
-            for r in results:
+            f = asyncio.gather(*[ContainerInfo.get_all(host, user, others=True) for host, user in hu_pairs])
+            ci_batches = await asyncio.wait_for(f, None)
+            for r in ci_batches:
                 table_data.extend(r)
 
         table = AsciiTable(table_data)
